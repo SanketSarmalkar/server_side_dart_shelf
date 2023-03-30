ed4afd9fe4eeff71ea46b8b58704048d53bfa337
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-03-30 07:43:35 +0530

    Static site

--- bin/server.dart
+++ bin/server.dart
@@ -2,11 +2,14 @@
 import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
 
 void main() async {
   var port = int.tryParse(Platform.environment['PORT'] ?? '') ?? 8080;
-  var server = await shelf_io.serve(
-      (Request request) => Response.ok("hello World!"), '0.0.0.0', port);
+  final handler = createStaticHandler('screens', defaultDocument: 'index.html');
+  // var server = await shelf_io.serve(
+  //     (Request request) => Response.ok("hello World!"), '0.0.0.0', port);
+  final server = await shelf_io.serve(handler, 'localhost', port);
   // print('Hello world: ${server.calculate()}!');
   print('Serving at http://${server.address.host}:${server.port}');
 }
